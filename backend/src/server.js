837567b4f41a9b4db188575f5c63acc34c90e32e
nishanthann <nizhanth23@gmail@gmail.com>
2025-11-21 03:16:57 +0530

    feat: integrate Clerk for user authentication and add MongoDB connection
    - Added Clerk authentication to the frontend with SignIn and UserButton components.
    - Removed unused App.css file and refactored App.jsx to simplify the UI.
    - Implemented MongoDB connection in the backend with mongoose.
    - Created User model for storing user data in MongoDB.
    - Set up Inngest functions for syncing and deleting users based on Clerk events.
    - Configured CORS and JSON parsing in the Express server.
    - Updated package.json and pnpm-lock.yaml to include new dependencies.
    - Added Vercel configuration files for backend and frontend deployments.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import "dotenv/config";
+import { connectDB } from "./libs/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./libs/inngest.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
+connectDB();
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Hello World" });
 });
-app.listen(process.env.PORT, () => console.log("Server ruing on port 3000"));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
