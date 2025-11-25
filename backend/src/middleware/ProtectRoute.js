2f34f0bb2ce57ff1ed50c79f040dac24c1327117
nishanthann <nizhanth23@gmail@gmail.com>
2025-11-25 07:38:22 +0530

    fix: improve error handling and user authentication in protectRoute middleware

--- backend/src/middleware/ProtectRoute.js
+++ backend/src/middleware/ProtectRoute.js
@@ -2,18 +2,24 @@ import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
 export const protectRoute = [
-  // followin requir auth is redirect to "/"
   requireAuth(),
   async (req, res, next) => {
     try {
-      const clerkId = req.auth().userId;
-      if (!clerkId) return res.status(401).json({ message: "Unauthorized" });
-      const user = await User.findOne({ clerkId });
-      if (!user) return res.status(401).json({ message: "Unauthorized" });
+      const { userId } = req.auth;
+
+      if (!userId) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      const user = await User.findOne({ clerkId: userId });
+      if (!user) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
       req.user = user;
       next();
     } catch (error) {
-      console.error("error in protected route");
+      console.error("error in protected route", error);
       res.status(500).json({ message: "Internal server error" });
     }
   },
